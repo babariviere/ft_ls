a315fb6aa44cf91c9c58855c46d77421d5c22303
Bastien <fourdotfiveg@gmail.com>
2017-11-24 17:18:33 +0100

    so much rec

--- src/io.c
+++ src/io.c
@@ -6,7 +6,7 @@
 /*   By: briviere <briviere@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/24 14:00:01 by briviere          #+#    #+#             */
-/*   Updated: 2017/11/24 14:02:04 by briviere         ###   ########.fr       */
+/*   Updated: 2017/11/24 17:00:28 by briviere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,36 @@ size_t	count_files(char *path)
 		count++;
 	closedir(dir);
 	return (count);
+}
+
+int		check_if_is_dir(char *path)
+{
+	struct stat		path_stat;
+
+	stat(path, &path_stat);
+	return ((path_stat.st_mode & S_IFDIR) == S_IFDIR);
+}
+
+char	**get_dir_rec(char *path, t_arg_opt *opt)
+{
+	DIR				*dir;
+	struct dirent	*ent;
+	char			**paths;
+	char			*joined;
+	size_t			idx;
+
+	dir = opendir(path);
+	paths = ft_memalloc(sizeof(char *) * (len + 1));
+	while ((ent = readdir(dir)) != 0)
+	{
+		if (ft_strcmp(ent->d_name, ".") == 0 ||
+				ft_strcmp(ent->d_name, "..") == 0)
+			continue ;
+		if (ent->d_name[0] == '.' && opt->hidden == 0)
+			continue ;
+		joined = ft_strjoin(path, ent->d_name);
+		if (check_if_is_dir(joined))
+			paths[idx++] = ft_strjoin(joined, ent->d_name);
+	}
+	closedir(dir);
 }
